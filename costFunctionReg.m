aeaa99e6fe0069d56f1983de376bb79607d439eb
wsccurry <32050295+wsccurry@users.noreply.github.com>
2017-12-18 20:51:23 +0800

    Update costFunctionReg.m

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -13,7 +13,7 @@ error1=-(y'*log(sigmoid(X*theta))+(1-y)'*log(1-sigmoid(X*theta)))/m;
 theta1=theta(2:n);
 error2=sum(theta(2:n).^2)*lambda/(2*m);
 J = error1+error2;
-grad=size(n,1);
+grad=ones(n,1);
 grad(1)=sum(sigmoid(X*theta)-y)/m;
 grad(2:n)=(X'(2:n,:)*(sigmoid(X*theta)-y))/m+lambda*theta(2:n)/m;
 
